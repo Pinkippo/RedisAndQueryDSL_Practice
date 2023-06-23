afcf6e424c08d14d35acc8431c91f07ec1650272
Seungwan Han <hseungwan8@gmail.com>
2023-06-23 11:20:46 +0900

    feat(prj): 프로젝트 생성

---
+++ Traffic/src/main/java/com/example/traffic/TrafficApplication.java
@@ -0,0 +1,13 @@
+package com.example.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficApplication.class, args);
+    }
+
+}
