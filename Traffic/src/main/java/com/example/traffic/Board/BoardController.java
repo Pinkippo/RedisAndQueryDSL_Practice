7c06461641ca8b11a9011e3b263b493a0e8dc75d
Seungwan Han <hseungwan8@gmail.com>
2023-07-04 22:25:55 +0900

    feat(api): 게시물 관련 로직 작성 (1)
    Board 관련 파일 생성

---
+++ Traffic/src/main/java/com/example/traffic/Board/BoardController.java
@@ -0,0 +1,13 @@
+package com.example.traffic.Board;
+
+public class BoardController {
+
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+
+
+}
---
+++ Traffic/src/main/java/com/example/traffic/Board/BoardService.java
@@ -0,0 +1,15 @@
+package com.example.traffic.Board;
+
+import com.example.traffic.Board.Repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+}
